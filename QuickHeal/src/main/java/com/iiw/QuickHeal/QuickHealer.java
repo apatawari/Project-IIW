b8d0630179c45aa6259699dbb60b97dfdb490d43
apatawari <patawari@usc.edu>
2015-11-15 14:44:52 -0800

    Updated wrapper for Home remedies ; Still need to extract text info

--- QuickHeal/src/main/java/com/iiw/QuickHeal/QuickHealer.java
+++ QuickHeal/src/main/java/com/iiw/QuickHeal/QuickHealer.java
@@ -1,21 +1,29 @@
 package com.iiw.QuickHeal;
 
 import com.usc.crawler.Controller;
+import com.usc.wrapper.Wrapper;
 
 /**
- * Hello world!
+ * @author Arihant
  *
  */
 public class QuickHealer 
 {
-    public static void main( String[] args )
+	private static String folderLocationHR="HomeRemedies";
+    public static void main( String[] args ) throws Exception
     {
-    	Controller crawl=new Controller();
-    	try {
-			crawl.startCrawlingHomeRemedies();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	//Crawls the links from the HomeRemedies (Source 1)
+    	//Outputs html files available in HomeRemedies folder
+    	//with list of illness in keywords.txt in homeremedies folder
+    	
+    	//Controller crawl=new Controller();
+    	//crawl.startCrawlingHomeRemedies();
+    	
+    	//It is wrapper to extract required content from the html files extracted above
+    	//generates the excel file in the 
+    	Wrapper wrapper=new Wrapper();
+    	wrapper.extractDataFromHtmlSource1(folderLocationHR);
+    	
+    	
     }
 }
